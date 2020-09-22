ffa1d7f4e12c1812821004ab2b52d32e911c600b
Pavel Poznyak <just_for_the_soul@mail.ru>
2020-09-23 01:13:18 +0200

    Первое задание

--- source/js/modules/social.js
+++ source/js/modules/social.js
@@ -1,9 +1,11 @@
 export default () => {
-  let socialBlock = document.querySelector(`.js-social-block`);
-  socialBlock.addEventListener(`mouseover`, function () {
-    socialBlock.classList.add(`social-block--active`);
-  });
-  socialBlock.addEventListener(`mouseleave`, function () {
-    socialBlock.classList.remove(`social-block--active`);
-  });
+  const socialBlock = document.querySelector(`.js-social-block`);
+  if (socialBlock) {
+    socialBlock.addEventListener(`mouseover`, function () {
+      socialBlock.classList.add(`social-block--active`);
+    });
+    socialBlock.addEventListener(`mouseleave`, function () {
+      socialBlock.classList.remove(`social-block--active`);
+    });
+  }
 };
